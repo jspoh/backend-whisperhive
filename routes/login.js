b393b710c855cb40ffc881f65deb2d81b33b7dd4
jspoh <jingsengyeah@gmail.com>
2023-01-01 18:03:21 +0800

    changed put to post for logging in

--- routes/login.js
+++ routes/login.js
@@ -3,6 +3,6 @@ const loginUser = require("../controllers/login").loginUser;
 
 const router = express.Router();
 
-router.route("/").put(loginUser);
+router.route("/").post(loginUser);
 
 module.exports = { router };
